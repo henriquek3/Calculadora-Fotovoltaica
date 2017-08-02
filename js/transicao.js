3f12a736d7149987774dabf2e46c9cd6e8c7cfc4
Jean Freitas <henriquek3@live.com>
2017-08-01 21:18:24 -0400

    transicao

--- js/transicao.js
+++ js/transicao.js
@@ -8,15 +8,36 @@ $(document).ready(function () {
     $('.right.button').click(() => {
         if (num <= 1 ) {
             num += 1;
-            console.log(views[num]);
+            switch(views[num]){
+                case('localizacao'):
+                    $('div.step:first').addClass('active');
+                    console.log('localizacao');break;
+                case('tipo'):
+                    $('div.step:odd').addClass('active');
+                    $('#localizacao').remove();
+                    $('#tipoLocal').transition('slide left','2000ms');
+                    console.log('tipo');break;
+                case('consumo'):
+                    $('div.step:last').addClass('active');
+                    $('#tipoLocal').remove();
+                    $('#consumo').transition('slide left','2000ms');
+                    console.log('consumo');break;
+            }
         }
     });
 
     $('.left.button').click(() => {
         if (num >= 1 ) {
             num -= 1;
-            console.log(views[num]);
+            switch(views[num]){
+                case('localizacao'):
+                    console.log('localizacao');break;
+                case('tipo'):
+                    console.log('tipo');break;
+                case('consumo'):
+                    console.log('consumo');break;
+            }
         }
-    })
+    });
 
 });
