0e9356286745ea119721a146d3851869f5835d55
Jean Freitas <henriquek3@live.com>
2017-08-08 23:25:26 -0400

    ajustado layout, hide main, show section

--- js/transicao.js
+++ js/transicao.js
@@ -2,26 +2,29 @@ $(document).ready(function () {
     let views = [
         'localizacao',
         'tipo',
-        'consumo'
+        'consumo',
+        'resultado'
     ];
     let num = 0;
     let tipolocal = '';
     let ufload = '';
     let uf = '';
     $('.right.button').click(() => {
-        if (num <= 1 ) {
+        if (num <= 2) {
             num += 1;
             switch(views[num]){
                 case('localizacao'):
                     $('div.step:first').addClass('active');
-                    console.log('localizacao');break;
+                    console.log('localizacao');
+                    break;
                 case('tipo'):
                     $('div.step:odd').addClass('active');
                     $('div.step:last').removeClass('disabled');
                     $('div.step:first').removeClass('active');
                     $('#localizacao').transition('hide');
                     $('#tipoLocal').transition('slide right', '2000ms');
-                    console.log('tipo');break;
+                    console.log('tipo');
+                    break;
                 case('consumo'):
                     $('div.step:odd').removeClass('active');
                     $('div.step:first').removeClass('active');
@@ -30,7 +33,13 @@ $(document).ready(function () {
                     $('div.step:last').addClass('active');
                     $('#tipoLocal').transition('hide');
                     $('#consumo').transition('slide right', '2000ms');
-                    console.log('consumo');break;
+                    console.log('consumo');
+                    break;
+                case('resultado'):
+                    console.log('resultado');
+                    $('main').transition('hide');
+                    $('section').transition('show');
+                    break;
             }
         }
     });
