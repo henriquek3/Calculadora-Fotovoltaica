4be71bb1d023cb75a94af3aa2392a924a3c4b8a4
Jean Freitas <henriquek3@live.com>
2017-09-08 15:11:57 -0400

    layout done, main.js in progress

--- js/main.js
+++ js/main.js
@@ -1,46 +1,47 @@
-$('select').dropdown();
+$(document).ready(function () {
+    let ufloaded = '';
 
+    /*
+     * @todo popular select estados
+     */
+    $('#uf').on('mouseenter', function () {
+        if (ufloaded === '') {
+            ufloaded = 'loaded';
+            $.ajax({
+                method: 'GET',
+                url: 'php/estados.php',
+                dataType: 'html'
+            })
+                .done(function (response) {
+                    $('#estados').html('<option> - selecione - </option>' + response);
+                    /*console.log(response);*/
+                })
+                .fail(function (response) {
+                    console.log(response);
+                });
+        }
+    });
 
-/*
- * @todo popular select estados
- */
-$('#uf').on('mouseenter', function () {
-    if (ufload === '') {
-        ufload = 'loaded';
+    /*
+     * @todo carregar cidades de acordo com o estado
+     */
+    $('#estados').change(function () {
+        uf = $('#estados').val();
+        /*console.log(uf);*/
         $.ajax({
             method: 'GET',
-            url: 'php/estados.php',
-            dataType: 'html'
+            url: 'php/cidades.php',
+            dataType: 'html',
+            data: {
+                'uf': uf
+            }
         })
             .done(function (response) {
-                $('#estados').html('<option> - selecione - </option>' + response);
-                /*console.log(response);*/
+                /* console.log(response); */
+                $('#cidades').html(response);
             })
-            .fail(function (response) {
-                console.log(response);
+            .fail(function (responseText) {
+                console.log(responseText);
             });
-    }
-});
-
-/*
- * @todo carregar cidades de acordo com o estado
- */
-$('#estados').change(function () {
-    uf = $('#estados').val();
-    /*console.log(uf);*/
-    $.ajax({
-        method: 'GET',
-        url: 'php/cidades.php',
-        dataType: 'html',
-        data: {
-            'uf': uf
-        }
-    })
-        .done(function (response) {
-            /* console.log(response); */
-            $('#cidades').html(response);
-        })
-        .fail(function (responseText) {
-            console.log(responseText);
-        });
+    });
 });
