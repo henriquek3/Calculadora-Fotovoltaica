4178a9308d654d6ae0f03a39ebdd50c6cdefd499
Jean Freitas <henriquek3@live.com>
2017-09-14 21:42:06 -0400

    ajustado estados

--- php/estados.php
+++ php/estados.php
@@ -6,13 +6,23 @@
  * Time: 12:24
  */
 
-//$conn = new PDO('mysql:host=localhost;dbname=luxsolar;charset=utf8', 'root', '84089554');
-$conn = new PDO('mysql:host=localhost;dbname=luxsolar;charset=utf8', 'root', '');
+$conn = new PDO('mysql:host=localhost;dbname=luxsolar;charset=utf8', 'root', '84089554');
+//$conn = new PDO('mysql:host=localhost;dbname=luxsolar;charset=utf8', 'root', '');
 
 $stmt = $conn->prepare("SELECT * FROM estados WHERE ativo = 'S' ORDER BY nome");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 print_r($result);
+echo '<hr>';
+
+$json = json_encode( $result );
+
+echo "<script> var teste = $json; console.log(teste);
+for (var key of teste) {
+	console.log(key.nome);
+}
+</script>";
+
 foreach ($result as $item) {
     echo '<option value="' . $item['uf_nome'] . '">' . $item['nome'] . '</option>';
 }
