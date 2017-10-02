42215111d627fe6d8afab8f8914c18bf8c3aee0e
Jean Freitas <henriquek3@live.com>
2017-10-02 15:40:21 -0400

    refatorando calculo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
     let ufloaded = '';
-
     /*
-     * @todo popular select estados
+     @todo popular select estados
      */
     $('#uf').on('mouseenter', function () {
         if (ufloaded === '') {
@@ -26,7 +25,7 @@ $(document).ready(function () {
     });
 
     /*
-     * @todo carregar cidades de acordo com o estado
+     @todo carregar cidades de acordo com o estado
      */
     $('#estados').change(function () {
         $uf = $(this);        
@@ -42,7 +41,7 @@ $(document).ready(function () {
             }
         })
         .done(function (response) {
-            let html = '';                        
+            let html = '<option value="nao-informado">Cidades</option>';
             for(let dados of response){
                 html += '<option value="' + dados.cidade_codigo + '">' + dados.nome + '</option>';
                 $('#cidades').html(html);
@@ -53,12 +52,41 @@ $(document).ready(function () {
         });
     });
 
+    /*
+     @todo ir para tela de resultados
+     */
     $('.ui.big.primary.button').click(function () {
         $('#header-calculadora').css('display', 'none');
         $('#corpo-calculadora').css('display', 'none');
 
         $('#header-resultado').css('display', 'block');
         $('#corpo-resultado').css('display', 'block');
-    })
-    
+    });
+
+    /*
+     @todo verifica se os campos foram preenchidos
+     */
+    $('#cidades').change(function () {
+        let $kwh = document.getElementById('kwh').value;
+        switch ($kwh) {
+            case '':
+                console.log($kwh + ' - case01');break;
+            case null:
+                console.log($kwh + ' - case02');break;
+            case NaN:
+                console.log($kwh);break;
+            default:
+                $('.ui.big.primary.button').removeClass('disabled');
+                console.log('class removed');
+        }
+    });
+    $('#kwh').change(function () {
+        let $cid = document.getElementById('cidades').value;
+        if ($cid !== 'nao-informado' && this.value > 1) {
+            console.log(this.value);
+            console.log($cid);
+            $('.ui.big.primary.button').removeClass('disabled');
+            console.log('removido class disabled do button ');
+        }
+    });
 });
