ad676609c2b4a5ecdcbb6d15c95ae95bd5085ac7
Jean Freitas <henriquek3@live.com>
2017-10-17 16:57:39 -0300

    testes ajustes vpl tir - alterado para um for

--- testes/js/main.js
+++ testes/js/main.js
@@ -24,39 +24,46 @@ calc = function calculosTirVpl(fvalorTarifa,
     let receitaAnual = 0;
     let custoOeM = 0;
     let receitaLiquidaAnual = 0;
-    let resultadoFinal = 0;
+    let resultadoFinal = 0;//cprecoMinOrcamento;
     let eneGerAnualA = 0;
     let eneGerAnualB = 0;
     let eneGerAnualC = 0;
     let resultEneGerAual = 0;
-    let tempA = 0;
-    let tempB = 0;
-    let tempC = 0;
-    let tempD = 0;
 
     custoOeM = taxaAnualOeM * (taxaInflacaoAnual * cprecoMinOrcamento );
-    /**
-     * @todo calcular tarifa
-     */
-    for (let ano = 0; ano < 24; ano++) {
-        fvalorTarifa += reajusteAnualTarifa * fvalorTarifa;
-        custoOeM = custoOeM + (0.07 * custoOeM);
-    }
 
     /**
      * @todo calcular energia gerada
      */
-    for (let ano = 0; ano < 23; ano++) {
+    for (let ano = 0; ano < 1; ano++) {
         eneGerAnualA = cenergiaGeradaAnual * perdaRendimentoAnualA;
         eneGerAnualB = cenergiaGeradaAnual * perdaRendimentoAnualB;
         eneGerAnualC = eneGerAnualB - eneGerAnualA;
+        console.log(ano,resultadoFinal.toPrecision(6),eneGerAnualC.toPrecision(5));
+        if (ano < 1){
+            resultadoFinal -= fvalorTarifa * eneGerAnualC;
+        }
+
         if (resultEneGerAual > 0) {
             eneGerAnualC = resultEneGerAual;
         }
         resultEneGerAual = eneGerAnualC * perdaRendimentoAnualB;
+        fvalorTarifa += reajusteAnualTarifa * fvalorTarifa;
+        custoOeM = custoOeM + (0.07 * custoOeM);
+        resultadoFinal -= fvalorTarifa * resultEneGerAual;
+        console.log(ano,resultadoFinal.toPrecision(6));
+        /**
+         * @todo terminar de calcular a tarifa
+         */
+         if(ano === 22) {
+             for (let anox = 0; anox < 1; anox++) {
+                 fvalorTarifa += reajusteAnualTarifa * fvalorTarifa;
+                 custoOeM = custoOeM + (0.07 * custoOeM);
+                 resultadoFinal -= fvalorTarifa * resultEneGerAual;
+                 console.log(anox,ano,resultadoFinal.toPrecision(6));
+             }
+         }
     }
-
-
     console.log({
         a: fvalorTarifa.toPrecision(3),
         b: parseInt(resultEneGerAual.toPrecision(5)),
@@ -65,5 +72,13 @@ calc = function calculosTirVpl(fvalorTarifa,
         e: (fvalorTarifa * parseInt(resultEneGerAual.toPrecision(5))) - custoOeM
     });
 };
-
 console.log(calc(0.75,11424,43651.65));
+/*
+
+a:"4.76"
+b:10078
+c:47929.81460242933
+d:553.5468421026877
+e:47376.26776032664
+
+ */
